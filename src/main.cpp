cb6a5f80688e60536234f80561d331745cce36c6
Sven Arweiler <dev@sba.io>
2017-11-30 19:58:15 +0100

    Edit mode prototype

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#include <main.h>
+
 #define OLED_D1     11 // MOSI
 #define OLED_D0     13 // CLK
 #define OLED_DC     12
@@ -11,6 +13,8 @@
 Adafruit_SSD1306 display(OLED_D1, OLED_D0, OLED_DC, OLED_RESET, OLED_CS);
 
 #define CLOCK_IN    18
+#define POT1_IN     19
+#define BUT1_IN     9
 
 #define NUMFLAKES 10
 #define XPOS 0
@@ -24,11 +28,15 @@ Adafruit_SSD1306 display(OLED_D1, OLED_D0, OLED_DC, OLED_RESET, OLED_CS);
 #define COL_NUM 4
 #define COL_WIDTH 16
 
+#define DISPLAY_HEIGHT 64
+#define DISPLAY_WIDTH 128
+
 #define SSD1306_LCDHEIGHT 64
 
 int activeSteps[COL_NUM] = {0, 0, 0, 0};
-int activeTriggers[COL_NUM] = {false, false, false, false};
+boolean activeTriggers[COL_NUM] = {false, false, false, false};
 int colHeights[COL_NUM] = {0, 0, 0, 0};
+boolean colEditable[COL_NUM] = {false, false, false, false};
 int colTriggerTargets[COL_NUM] = {0, 1, 2, 3};
 
 void activateTrigger(int colNum) {
@@ -57,19 +65,39 @@ void initCol(int colNum, int height) {
 }
 
 void updateCol(int colNum) {
-  int x = activeSteps[colNum] * STEP_HEIGHT;
-  int y = colNum * COL_WIDTH;
-  display.fillRect(x, y, 14, 14, 0);
-  display.drawRect(x, y, 14, 14, 1);
+  if(!isInEditMode(colNum)) {
+    int x = activeSteps[colNum] * STEP_HEIGHT;
+    int y = colNum * COL_WIDTH;
+    display.fillRect(x, y, 14, 14, 0);
+    display.drawRect(x, y, 14, 14, 1);
 
-  if(activeSteps[colNum] == 0) {
-    activeSteps[colNum] = colHeights[colNum] + 1;
-    activateTrigger(colTriggerTargets[colNum]);
+    if(activeSteps[colNum] == 0) {
+      activeSteps[colNum] = colHeights[colNum] + 1;
+      activateTrigger(colTriggerTargets[colNum]);
+    }
+
+    int new_x = (activeSteps[colNum] - 1) * STEP_HEIGHT;
+    display.fillRect(new_x, y, 14, 14, 1);
+    activeSteps[colNum] -= 1;
   }
+}
 
-  int new_x = (activeSteps[colNum] - 1) * STEP_HEIGHT;
-  display.fillRect(new_x, y, 14, 14, 1);
-  activeSteps[colNum] -= 1;
+void blankCol(int colNum) {
+  int x = 0;
+  int y = colNum * COL_WIDTH;
+  display.fillRect(x, y, DISPLAY_WIDTH, COL_WIDTH, 0);
+}
+
+void drawEmptyCol(uint8_t colNum, uint8_t height) {
+  int y = colNum * COL_WIDTH;
+  for(int i=0; i < height; i++) {
+      int x = i * STEP_HEIGHT;
+      display.drawRect(x, y, 14, 14, 1);
+  }
+}
+
+boolean isInEditMode(int colNum) {
+  return colEditable[colNum];
 }
 
 void onClockReceived() {
@@ -83,11 +111,23 @@ void onClockReceived() {
   Serial.println("CLOCK IN");
 }
 
+void onButton1Pressed() {
+  uint8_t colNum = 0;
+
+  Serial.println("BUTTON 1 pressed");
+  colEditable[colNum] = !colEditable[colNum];
+  blankCol(colNum);
+}
+
 void setup()   {
   Serial.begin(9600);
 
   pinMode(CLOCK_IN, INPUT);
+  pinMode(BUT1_IN, INPUT);
+
   attachInterrupt(CLOCK_IN, onClockReceived, RISING);
+  attachInterrupt(BUT1_IN, onButton1Pressed, FALLING);
+
 
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC);
@@ -105,15 +145,9 @@ void setup()   {
 }
 
 void loop() {
-  /*
-  updateCol(0);
-  updateCol(1);
-  updateCol(2);
-  updateCol(3);
-  fireTriggers();
-  resetTriggers();
+  int pot1_val = analogRead(POT1_IN);
+  Serial.println(pot1_val);
 
-  display.display();
-  delay(500);
-  */
+  onClockReceived();
+  delay(pot1_val);
 }
